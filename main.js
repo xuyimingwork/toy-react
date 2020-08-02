3a21462e5a53b694a68dab5978c5c442ea4c330e
xuyimingwork <xuyimingwork@foxmail.com>
2020-08-02 16:25:50 +0800

    处理 jsx 中的原生元素

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import React from './toy-react'
+import ReactDOM from './toy-react'
 
-const div = <div>Hello World</div>
+const div = <div id="id">
+  <div class="class">mmm</div>
+  Hello World
+</div>
+
+ReactDOM.render(div, document.getElementById('root'));
--- toy-react.js
+++ toy-react.js
@@ -1,5 +1,23 @@
 export default {
-  createElement() {
-    console.log('createElement', ...arguments)
+  createElement(type, props, ...children) {
+    console.log('createElement', type, props, children)
+
+    const element = document.createElement(type)
+    for (let name in props) {
+      element.setAttribute(name, props[name])
+    }
+    for (let child of children) {
+      if (typeof child === 'string') {
+        child = document.createTextNode(child)
+      }
+      element.appendChild(child)
+    }
+
+    return element
+  },
+
+  render(element, container) {
+    console.log('render', element, container)
+    container.appendChild(element)
   }
 }
